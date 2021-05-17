b53f9cbf13b00787f8b3cc071e19611014b950c3
Max Tiutmanov <max.tiutmanov@bookedby.com>
2021-05-17 09:56:50 +0300

    dcp62

---
+++ vs/dcp/dcp/Dcp62.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dcp
+{
+    /*
+     *  This problem was asked by Facebook.
+
+        There is an N by M matrix of zeroes. Given N and M, write a function to count the number of ways of starting at the 
+        top-left corner and getting to the bottom-right corner. You can only move right or down.
+
+        For example, given a 2 by 2 matrix, you should return 2, since there are two ways to get to the bottom-right:
+
+        Right, then down
+        Down, then right
+        Given a 5 by 5 matrix, there are 70 ways to get to the bottom-right.
+     */
+
+    public static class Dcp62
+    {
+        public static long GetNumberOfOptions(int n, int m)
+        {
+            var cache = new long[n, m];
+            return GetNumberOfOptionsDp(n - 1, m - 1, cache);
+        }
+
+        private static long GetNumberOfOptionsDp(int n, int m, long[,] cache)
+        {
+            if (n == 0 && m == 0)
+                return 1;   // there is only one "option" to start at 0,0 
+
+            if (n < 0 || m < 0)
+                return 0;
+
+            if (cache[n, m] > 0)
+                return cache[n, m];
+
+            // sum of options if we got here from above PLUS if we got here from left
+            var totalOptions = GetNumberOfOptionsDp(n - 1, m, cache) + GetNumberOfOptionsDp(n, m - 1, cache);
+            cache[n, m] = totalOptions;
+
+            return totalOptions;
+        }
+
+        private static long GetNumberOfOptionsNaive(int n, int m)
+        {
+            // n - num of columns, m - num of rows
+
+            if (n > 1 && m > 1)
+            {
+                // what if we 1) move right? 2) move down? 
+                return GetNumberOfOptionsNaive(n - 1, m) + GetNumberOfOptionsNaive(n, m - 1);
+            }
+
+            return 1; // there is only 1 way to finish the path - only right or only down
+        }
+    }
+}
--- vs/dcp/dcp/Program.cs
+++ vs/dcp/dcp/Program.cs
@@ -6,13 +6,9 @@ namespace dcp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("2^10 = {0}", Dcp61.Pow(2, 10));
-            Console.WriteLine("3^15 = {0}", Dcp61.Pow(3, 15));
-            Console.WriteLine("2^31 = {0}", Dcp61.Pow(2, 31));
-            Console.WriteLine("2^57 = {0}", Dcp61.Pow(2, 57));
-            Console.WriteLine("1^1577 = {0}", Dcp61.Pow(1, 1577));
-            Console.WriteLine("1^157700 = {0}", Dcp61.Pow(1, 157700));
-
+            Console.WriteLine("For 2x2 matrix: {0}", Dcp62.GetNumberOfOptions(2, 2));
+            Console.WriteLine("For 5x5 matrix: {0}", Dcp62.GetNumberOfOptions(5, 5));
+            Console.WriteLine("For 100x100 matrix: {0}", Dcp62.GetNumberOfOptions(100, 100));
             Console.ReadLine();
         }
     }
